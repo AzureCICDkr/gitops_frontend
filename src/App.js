2f83aae68f1aae3734744597554decd33e412f7d
seohan1010 <seohan1010@hotmail.com>
2025-12-04 05:09:08 +0900

    google login button added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import apiClient from './api.js';
 
 function App() {
+
+
+const data = apiClient.get('/')
+             .then(response => console.log(response.data))
+	     .catch(error => console.log(error))	
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        !
+
+
+     <button onClick={() => window.location.href = "http://localhost:8888/oauth2/authorization/google"}>
+         Google Login
+     </button>
+
+</header>
+
     </div>
   );
 }
